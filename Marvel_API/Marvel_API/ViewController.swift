e80b2b0ba62c22bb9e8135a997d3b6e7da0ddee1
iOSLab <iOSLab@iOSLab-18.local>
2024-06-22 16:02:55 -0300

    Delegate Criado

--- Marvel_API/Marvel_API/ViewController.swift
+++ Marvel_API/Marvel_API/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import 
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
 }
-
